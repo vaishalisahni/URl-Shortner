d16a49d80b28ab293a28c23aafa760dac7bbca93
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-07-09 00:29:47 +0530

    dfgsd

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -4,12 +4,12 @@ const URL = require("../models/url")
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-if(!req.user) return res.redirect("/login");
+  if (!req.user) return res.redirect("/login");
 
   // const allUrls = await URL.find({}); // before login signup logic
 
   // ṇow we will show only those links which are created by the user
-  const allUrls= await URL.find({createdBy: req.user._id})
+  const allUrls = await URL.find({ createdBy: req.user._id })
   res.render('home', {
     urls: allUrls,
   });
